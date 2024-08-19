c2d55e5c552c68fe8890f65fcc959c15346cb959
Justyna Kwaśniak <jkwasniak193@gmail.com>
2024-08-19 19:35:52 +0200

    update day 2

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,7 +8,18 @@ const Navbar = () => {
         <Link className="navbar-brand" to="/">
           Home
         </Link>
-        <div className="collapse navbar-collapse">
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
               <Link className="nav-link" to="/about">
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,25 +1,43 @@
+import React from "react";
+import flowerImage from "../images/photo-flower.avif";
+
 const Home = () => {
   return (
-    <div className="container">
-      <div className="row">
-        <div>
-          <h1 className="text-center mb-custom custom-heading mt-5">
-            <em>
-              Welcome to my place - dedicated to delivering excellence and
-              creativity
-            </em>
-          </h1>
-          <p className="text-center col-md-6 mx-auto">
-            I’m Justyna, a passionate Front-End Developer with a keen eye for
-            UX/UI design. While my professional journey is just beginning, I’ve
-            spent countless hours crafting elegant, user-centric applications
-            that merge form and function. My background in journalism and
-            hands-on experience in diverse fields have honed my problem-solving
-            skills and creative vision. Join me as I bring innovative web
-            solutions to life, one pixel at a time.
-          </p>
+    <div>
+      <div className="container">
+        <div className="row align-items-center">
+          {/* Sekcja z tekstem po lewej stronie */}
+          <div className="col-md-6 mt-5">
+            <h1 className="mb-custom custom-heading text-center mb-5">
+              <em>
+                Welcome to my place - dedicated to delivering excellence and
+                creativity
+              </em>
+            </h1>
+            <p className="text-center">
+              I’m Justyna, a passionate Front-End Developer with a keen eye for
+              UX/UI design. While my professional journey is just beginning,
+              I’ve spent countless hours crafting elegant, user-centric
+              applications that merge form and function. My background in
+              journalism and hands-on experience in diverse fields have honed my
+              problem-solving skills and creative vision. Join me as I bring
+              innovative web solutions to life, one pixel at a time.
+            </p>
+          </div>
+
+          {/* Sekcja z obrazem po prawej stronie */}
+          <div className="col-md-6 mt-5">
+            <img src={flowerImage} alt="Creative Work" className="img-fluid" />
+          </div>
         </div>
       </div>
+
+      {/* Stopka */}
+      <footer className="py-4 mt-5">
+        <div className="container text-center">
+          <p className="mb-0">© 2024 Made by Justyna Kwaśniak with love </p>
+        </div>
+      </footer>
     </div>
   );
 };
